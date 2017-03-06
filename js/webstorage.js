27694e970dce32bec5ba31873d320024f6fd22e1
Jakob Ferdinandsen <jakob@jhaaf.dk>
2017-03-06 10:49:40 +0100

    Added jsNotify on every 10th visit

--- js/webstorage.js
+++ js/webstorage.js
@@ -4,17 +4,22 @@
 $(function () {
     localStorage.setItem(Date.now(), new Date());
     populateVisits();
+
+    if (localStorage.length % 10 == 0){
+        $.notify("This is your "+localStorage.length+"th visit", {position:"right bottom", className:"success"});
+    }
 });
 
 function populateVisits() {
-    for (var i = 0; i < localStorage.length; i++) {
+    for (var i = localStorage.length-1; i >= 0; i--) {
         $("#tableBody").append(getTR(localStorage.key(i), localStorage.getItem(localStorage.key(i))));
     }
 }
 
 function getTR(timestamp, date) {
     return "<tr>" +
-        "<td>" + date + "</td>" +
+        "<td>" + new Date(date).toLocaleDateString("da-DK") + "</td>" +
+        "<td>" + new Date(date).toTimeString() + "</td>" +
         "<td>" + timestamp + "</td>" +
         "</tr>";
 }
