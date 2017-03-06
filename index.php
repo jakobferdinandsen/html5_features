5254fa56b336f2f58f78ce0e2cdac72a039d5816
Jakob Ferdinandsen <jakob@jhaaf.dk>
2017-03-06 13:00:27 +0100

    Added svg example

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php include("html/layout.html"); ?>
+    <div class="text-center">
+        <h2>SVG Example</h2>
+
+        <svg width="320" height="500">
+            <rect x="0" y="20" rx="10" ry="10" width="320" height="180"
+                  style="fill:black"></rect>
+            <rect x="20" y="38" width="280" height="144"
+                  style="fill:white;stroke:gray;stroke-width:2"></rect>
+            <rect x="0" y="200" width="320" height="25" rx="10" ry="10" style="fill:gray"></rect>
+        </svg>
+    </div>
+
 <?php include("html/close.html"); ?>
