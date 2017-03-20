86f943c144610bb8db48a20344a034c6dceafc76
Jakob Ferdinandsen <jakob@jhaaf.dk>
2017-03-20 16:31:18 +0100

    Added ollie POC

---
+++ ollie.php
@@ -0,0 +1,4 @@
+<?php include("html/layout.html"); ?>
+    <p class="text-center">Demonstration of using osc.js & cylon.js to control a Sphero Ollie through the Open Sound Control protocol. This is just a proof of concept</p>
+      <iframe class="center-block" src="https://drive.google.com/file/d/0BziJsUUUOnhmVW5mTjFoNUZSd0k/preview" width="1280" height="720"></iframe>
+<?php include("html/close.html"); ?>
